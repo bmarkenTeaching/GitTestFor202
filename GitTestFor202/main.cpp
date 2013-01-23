883b89d765f598bffc39e74cb3c798539d1da289
Brandon Marken <brandon.marken@gmail.com>
2013-01-23 14:31:34 -0900

    Initial Commit

---
+++ GitTestFor202/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GitTestFor202
+//
+//  Created by Brandon Marken on 1/23/13.
+//  Copyright (c) 2013 Brandon Marken. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
